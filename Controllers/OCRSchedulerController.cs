802ff52ff6c8d825c7ff76e6c66e89296e6e93b3
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-09-21 21:16:08 +0700

    Add project files.

---
+++ Controllers/OCRSchedulerController.cs
@@ -0,0 +1,18 @@
+﻿using etl_job_service.DataTransferObject;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace etl_job_service.Controllers
+{
+    [Route("api/ocr")]
+    [ApiController]
+    public class OCRSchedulerController : ControllerBase
+    {
+        [HttpPost("test")]
+        public BaseResponse<int?> Test()
+        {
+             return BaseResponse<int?>.Ok("Successfully running your first API using .NET Core", 100);
+        }
+
+    }
+}
---
+++ DataTransferObject/BaseResponse.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace etl_job_service.DataTransferObject
+{
+    /**
+     * BaseResponse
+     * 
+     * This data transfer object (DTO) used for standardizing the response.
+     */
+    public class BaseResponse<T>
+    {
+        public bool status { get; set; }
+        public int code { get; set; }
+        public string message { get; set; }
+        public T data { get; set; }
+
+        public BaseResponse(bool status, int code, string message, T data) 
+        { 
+            this.status = status;
+            this.code = code;
+            this.message = message;
+            this.data = data;
+        }
+
+        public static BaseResponse<T> Ok(string message, T data)
+        {
+            return new BaseResponse<T>(true, 200, message, data);
+        }
+
+        public static BaseResponse<T> Error(string message, T data) 
+        {
+            return new BaseResponse<T> (false, 500, message, data);
+        }
+    }
+}
