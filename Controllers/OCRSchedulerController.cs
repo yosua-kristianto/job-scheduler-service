b9861d150ef5eb363fced8d4a216df77e9bc521d
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-09-25 00:28:19 +0700

    Change the way OCRSchedulerController being presented by implementing the Dependency Injection (DI) pattern.

--- Controllers/OCRSchedulerController.cs
+++ Controllers/OCRSchedulerController.cs
@@ -1,17 +1,34 @@
-﻿using etl_job_service.DataTransferObject;
+﻿using etl_job_service.DTO;
+using etl_job_service.Entity.Img;
+using etl_job_service.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace etl_job_service.Controllers
 {
+    /**
+     * OCRSchedulerController
+     * 
+     * This controller supposed to used for OCR Scheduler related functionalities
+     */ 
     [Route("api/ocr")]
     [ApiController]
     public class OCRSchedulerController : ControllerBase
     {
-        [HttpPost("test")]
-        public BaseResponse<int?> Test()
+        private readonly IImageProfileRepository repository;
+
+        public OCRSchedulerController(IImageProfileRepository repository)
         {
-             return BaseResponse<int?>.Ok("Successfully running your first API using .NET Core", 100);
+            this.repository = repository;
+        }
+
+        [HttpPost("test")]
+        public BaseResponse<List<ImageProfile>> Test()
+        {
+             return BaseResponse<List<ImageProfile>>.Ok(
+                 "Successfully running your first API using .NET Core", 
+                 repository.GetAll()
+             );
         }
 
     }
