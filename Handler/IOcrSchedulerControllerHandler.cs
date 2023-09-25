fc864c7f1b4bcb3ab19e73a6a6cf44565cc39fbf
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-09-25 21:21:59 +0700

    Completing UploadImageSync and Async for non Zip scenario

---
+++ Handler/IOcrSchedulerControllerHandler.cs
@@ -0,0 +1,57 @@
+﻿using etl_job_service.DTO.Request.OcrScheduler;
+using etl_job_service.DTO.Response.OcrScheduler;
+
+namespace etl_job_service.Handler
+{
+    public interface IOcrSchedulerControllerHandler
+    {
+        /**
+         * @public
+         * UploadImageHandler
+         * 
+         * This API handle Upload Image with method below:
+         * 
+         * 1. Determine whatever the uploaded file is png / jpg / zip
+         *      1.1 If The file is png or jpg
+         *          1.1.1   Generate new name for the file with this format below
+         *                  {FILE_EXTENSION}#{YmdHis}
+         *          1.1.2   Rename the file as the 1.1.1
+         *          1.1.3   Generate `ImageProfile` class
+         *          1.1.4   Set image_original_name in 1.1.3 as `request.Image.Name`
+         *          1.1.5   Set image_server_name in 1.1.3 as 1.1.1
+         *          1.1.6   Set expected_ouput in 1.1.3 as `ExpectedOutput`
+         *          1.1.7   Prepare `JobScheduler` class
+         *          1.1.8   If the `InstantUpload` is 0
+         *              1.1.8.1     Set schedule_date in 1.1.7 as `ScheduleDate`          
+         *              1.1.8.2     Set is_synchronous in 1.1.7 as 0
+         *          1.1.9   Else
+         *              1.1.9.1     Set schedule_date in 1.1.7 as NOW()
+         *          1.1.10  Save the 1.1.3 and 1.1.7
+         *          1.1.11  Generate `MapImageProfileJobScheduler` class
+         *          1.1.12  Save `image_id` in 1.1.11 as 1.1.3
+         *          1.1.13  Save `schedule_id` in 1.1.11 as 1.1.7
+         *          1.1.14  Save the 1.1.11
+         *          1.1.15  Move the file to location that being registered in appsettings.json as DefaultFileStoragePath 
+         *          1.1.16  If the `InstantUpload` is 0
+         *              1.1.16.1    Call out Tesseract Instance (Other Project)
+         *      1.2 If the file is zip
+         *          1.2.1   Extract the image file within zip
+         *          1.2.2   For every file within the extracted zip
+         *              1.2.2.1 Do 1.1 for the file
+         *              
+         * @throws
+         * OCR0001
+         * OCR0002
+         */
+        public UploadImageSync UploadImageHandler(UploadImageSyncAsync request);
+
+        /**
+         * @async
+         * @public
+         * AsyncUploadImageHandler
+         * 
+         * This function handle call to UploadImageHandler Asynchronously.
+         */
+        public Task AsyncUploadImageHandler(UploadImageSyncAsync request);
+    }
+}
---
+++ Handler/OcrSchedulerControllerHandler.cs
@@ -0,0 +1,98 @@
+﻿using etl_job_service.Config;
+using etl_job_service.DTO.Request.OcrScheduler;
+using etl_job_service.DTO.Response.OcrScheduler;
+using etl_job_service.Entity.Img;
+using etl_job_service.Entity.Job;
+using etl_job_service.Exceptions.Ocr;
+using etl_job_service.Repository.Img;
+using etl_job_service.Repository.Job;
+
+namespace etl_job_service.Handler
+{
+    public class OcrSchedulerControllerHandler : IOcrSchedulerControllerHandler
+    {
+        private readonly IImageProfileRepository imageRepository;
+        private readonly IJobRepository jobRepository;
+        private readonly IJobSchedulerRepository jobSchedulerRepository;
+
+        public OcrSchedulerControllerHandler(IImageProfileRepository imageRepository, IJobSchedulerRepository jobSchedulerRepository, IJobRepository jobRepository)
+        {
+            this.imageRepository = imageRepository;
+            this.jobSchedulerRepository = jobSchedulerRepository;
+            this.jobRepository = jobRepository;
+        }
+
+        /**
+         * @public
+         * OneByOneFileOperation
+         * 
+         * This function will actually do the operation one by one as stated on the Interface regarding the UploadImageHandler
+         * 
+         * @param
+         *  request
+         *      => Taken from controller
+         *  fileExtension
+         *      => Taken from UploadImageHandler function.
+         */
+        private UploadImageSync OneByOneFileOperation(UploadImageSyncAsync request, string fileExtension)
+        {
+            // Call out 1.1.3 to 1.1.6
+            ImageProfile imageProfile;
+            
+            string fileServerName = fileExtension.ToUpper() + "#" + DateTime.Now.ToString("YMdHms") + "." + fileExtension;
+            if (request.ExpectedOutput != null)
+            {
+                imageProfile = this.imageRepository.Create(request.Image.FileName, fileServerName, request.ExpectedOutput);
+            }else
+            {
+                imageProfile = this.imageRepository.Create(request.Image.FileName, fileServerName);
+            }
+
+            // Call out 1.1.7 to 1.1.10
+            DateTime scheduleTime = DateTime.Now;
+
+            if(request.InstantUpload == 1 && request.ScheduleTime != null)
+            {
+                scheduleTime = (DateTime) request.ScheduleTime;
+            }
+
+            JobScheduler jobScheduler = this.jobSchedulerRepository.Create(scheduleTime, request.InstantUpload == 1);
+
+            // Call out 1.1.14
+            MapImageProfileJobScheduler job = this.jobRepository.Create(jobScheduler.id, imageProfile.id);
+
+            // Call out 1.1.15
+            string pathToStorage = EnvironmentVariables.GetInstance().Key("default-file-storage-path");
+
+            FileStorage.WriteFile(pathToStorage, request.Image, (fileServerName));
+
+            return new UploadImageSync() { JobId = job.id };
+        }
+
+        public async Task AsyncUploadImageHandler(UploadImageSyncAsync request)
+        {
+            this.UploadImageHandler(request);
+        }
+
+        public UploadImageSync UploadImageHandler(UploadImageSyncAsync request)
+        {
+            string fileExtension = Path.GetExtension(request.Image.FileName).ToLower();
+
+            switch (fileExtension)
+            {
+                case ".png":
+                case ".jpg":
+                    this.OneByOneFileOperation(request, fileExtension);
+                    break;
+
+                case ".zip":
+                    throw new Exception("Zip operation is not implemented yet.");
+                    break;
+                default:
+                    throw new UploadedFileWrongExtensionException();
+            }
+
+            return null;
+        }
+    }
+}
