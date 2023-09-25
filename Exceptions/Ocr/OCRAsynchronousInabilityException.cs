aa751f5ff4df6a0da83647016a4ba17d37378fdd
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-09-25 21:21:27 +0700

    Added Exception layer

---
+++ Exceptions/Ocr/OCRAsynchronousInabilityException.cs
@@ -0,0 +1,11 @@
+﻿namespace etl_job_service.Exceptions.Ocr
+{
+    public class OCRAsynchronousInabilityException: BaseException
+    {
+        public OCRAsynchronousInabilityException() 
+        {
+            this.code = "OCR0002";
+            this.message = "To enable direct OCR operation, you must not upload Zip operation or you will have the consequences.";
+        }
+    }
+}
