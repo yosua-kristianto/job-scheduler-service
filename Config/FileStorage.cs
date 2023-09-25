ed9ef32de4b9321b426232ef1e26959f13b1884e
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-09-25 21:20:45 +0700

    Added File Storage helper

---
+++ Config/FileStorage.cs
@@ -0,0 +1,34 @@
+﻿namespace etl_job_service.Config
+{
+    public class FileStorage
+    {
+        /**
+         * @void 
+         * @static
+         * @public
+         * WriteFile
+         * 
+         * @param
+         * - path => Folder path
+         * 
+         * This function will move file to destinated path with this methodology below:
+         * 1. If directory not exist
+         *      1.1     Create directory 
+         * 2. Write the file to destinated path.
+         */
+        public static void WriteFile(string path, IFormFile file, string targetFileName)
+        {
+            string pathTarget = @path;
+
+            // Call off step 1 to 1.1
+            if (!Directory.Exists(pathTarget))
+            {
+                Directory.CreateDirectory(pathTarget);
+            }
+
+            // Call off step 2
+            Stream fileStream = new FileStream(Path.Combine(pathTarget, targetFileName), FileMode.Create);
+            file.CopyTo(fileStream);
+        }
+    }
+}
