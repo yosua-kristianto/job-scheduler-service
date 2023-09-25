2952f686492b4bdee21f86cc52a289536a35e717
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-09-25 21:18:40 +0700

    Completing Entity

--- Entity/Job/MapImageProfileJobScheduler.cs
+++ Entity/Job/MapImageProfileJobScheduler.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace etl_job_service.Entity.Job
 {
@@ -6,12 +7,18 @@ namespace etl_job_service.Entity.Job
      * MapImageProfileJobScheduler
      * 
      * @version 0.0.1
-     * 
-     * @todo Make this snake_case or try to serialize this as snake_case on response.
-     * @todo Map this out
      */
     [Table("job_map_image_profile_job_scheduler_mapper")]
     public class MapImageProfileJobScheduler
     {
+        [Key]
+        [Column("job_id")]
+        public string id { get; private set; }
+
+        [Column("schedule_id")]
+        public string scheduleId { get; set; }
+
+        [Column("image_id")]
+        public string imageId { get; set; }
     }
 }
--- Entity/View/OcrResultRecap.cs
+++ Entity/View/OcrResultRecap.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace etl_job_service.Entity.View
 {
@@ -6,12 +7,25 @@ namespace etl_job_service.Entity.View
      * OcrResultRecap
      * 
      * @version 0.0.1
-     * 
-     * @todo Make this snake_case or try to serialize this as snake_case on response.
-     * @todo Map this out
      */
     [Table("ocr_view_image_result_recap")]
     public class OcrResultRecap
     {
+        [Column("image_original_name")]
+        [Key]
+        public string imageOriginalName { get; private set; }
+
+        [Column("image_in_server_name")]
+        public string imageServerName { get; private set; }
+
+        [Column("result")]
+        public string result { get; private set; }
+
+        [Column("cer_point")]
+        public decimal cer { get; private set; }
+
+        [Column("report_issued_date")]
+        public string report_issued_date { get; private set; }
+
     }
 }
