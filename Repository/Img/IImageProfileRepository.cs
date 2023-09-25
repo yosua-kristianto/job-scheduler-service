91e6f94d6ea607d2060bd49cc1d366fd2a156423
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-09-25 21:16:03 +0700

    Moving this to /Repository/Img folder

---
+++ Repository/Img/IImageProfileRepository.cs
@@ -0,0 +1,45 @@
+﻿using etl_job_service.Entity.Img;
+
+namespace etl_job_service.Repository.Img
+{
+    /**
+     * @repository
+     * ImageProfileRepository
+     * 
+     * This class contains every query needed within ImageProfile
+     */
+    public interface IImageProfileRepository
+    {
+        /**
+         * @public 
+         * GetAll
+         * 
+         * This function will return all data from ImageProfile table.
+         */
+        public List<ImageProfile> GetAll();
+
+        /**
+         * @public
+         * Create
+         * 
+         * This function will create a new ImageProfile data for the case of no expectedOutput
+         */ 
+        public ImageProfile Create(string originalName, string inServerName);
+
+        /**
+         * @public
+         * Create
+         * 
+         * This function will create a new ImageProfile data.
+         */
+        public ImageProfile Create(string originalName, string inServerName, string expectedOutput);
+
+        /**
+         * @public
+         * GetDataById
+         * 
+         * This function will return a data of ImageProfile by providing its id.
+         */ 
+        public ImageProfile GetDataById(string id);
+    }
+}
---
+++ Repository/Img/ImageProfileRepository.cs
@@ -0,0 +1,43 @@
+﻿using etl_job_service.Config;
+using etl_job_service.Entity.Img;
+
+namespace etl_job_service.Repository.Img
+{
+    public class ImageProfileRepository : BaseRepository, IImageProfileRepository
+    {
+        public ImageProfileRepository(DatabaseContext context) : base(context) { }
+
+        public ImageProfile Create(string originalName, string inServerName)
+        {
+            ImageProfile imageProfile = new ImageProfile();
+
+            imageProfile.inServerName = inServerName;
+            imageProfile.originalName = originalName;
+
+            this._context.ImageProfile.Add(imageProfile);
+            this._context.SaveChanges();
+
+            return this.GetDataById(imageProfile.id);
+        }
+
+        public ImageProfile Create(string originalName, string inServerName, string expectedOutput)
+        {
+            ImageProfile imageProfile = this.Create(originalName, inServerName);
+            imageProfile.expectedOutput = expectedOutput;
+
+            this._context.SaveChanges();
+            return this.GetDataById(imageProfile.id);
+        }
+
+        public List<ImageProfile> GetAll()
+        {
+            Console.WriteLine("This function has been called");
+            return this._context.ImageProfile.ToList();
+        }
+
+        public ImageProfile GetDataById(string id)
+        {
+            return this._context.ImageProfile.FirstOrDefault(e => e.id == id);
+        }
+    }
+}
