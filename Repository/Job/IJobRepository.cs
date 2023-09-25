e9e721f6eb7c6839336f2a714de91efcad4435cc
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-09-25 21:17:54 +0700

    Added Job and JobScheduler Repository

---
+++ Repository/Job/IJobRepository.cs
@@ -0,0 +1,21 @@
+﻿using etl_job_service.Entity.Job;
+
+namespace etl_job_service.Repository.Job
+{
+    public interface IJobRepository
+    {
+        /**
+         * GetDataById
+         * 
+         * This function will return the Job data by provided id.
+         */
+        public MapImageProfileJobScheduler GetDataById(string id);
+
+        /**
+         * Create
+         * 
+         * This function will create new Job data.
+         */
+        public MapImageProfileJobScheduler Create(string scheduleId, string imageId);
+    }
+}
---
+++ Repository/Job/JobSchedulerRepository.cs
@@ -0,0 +1,27 @@
+﻿using etl_job_service.Config;
+using etl_job_service.Entity.Job;
+
+namespace etl_job_service.Repository.Job
+{
+    public class JobSchedulerRepository : BaseRepository, IJobSchedulerRepository
+    {
+        public JobSchedulerRepository(DatabaseContext context) : base(context){}
+
+        public JobScheduler Create(DateTime scheduleTime, bool sync)
+        {
+            JobScheduler jobScheduler = new JobScheduler();
+            jobScheduler.scheduleTime = scheduleTime;
+            jobScheduler.synchronousOperation = sync;
+
+            this._context.JobScheduler.Add(jobScheduler);
+            this._context.SaveChanges();
+
+            return this.GetDataById(jobScheduler.id);
+        }
+
+        public JobScheduler GetDataById(string id)
+        {
+            return this._context.JobScheduler.FirstOrDefault(e => e.id == id);
+        }
+    }
+}
