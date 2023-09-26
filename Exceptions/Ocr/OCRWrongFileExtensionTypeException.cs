617c632b72881e807b6b23b09711b4bd060839eb
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-09-26 19:41:01 +0700

    Added OCRWrongFileExtensionTypeException

---
+++ Exceptions/Ocr/OCRWrongFileExtensionTypeException.cs
@@ -0,0 +1,11 @@
+﻿namespace etl_job_service.Exceptions.Ocr
+{
+    public class OCRWrongFileExtensionTypeException : BaseException
+    {
+        public OCRWrongFileExtensionTypeException(string unexpectedExtension)
+        {
+            this.code = "OCR0003";
+            this.message = "The OCR system can only process file with extension of .jpg or .png. Getting " + unexpectedExtension;
+        }
+    }
+}
